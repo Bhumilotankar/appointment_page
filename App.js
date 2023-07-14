a6dbc9c82f6ad2aa3a44e9edbad6166adce286e3
Bhumilotankar <129989145+Bhumilotankar@users.noreply.github.com>
2023-07-14 16:22:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import Main from './screens/Main'
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import What_Doctor_says from './screens/slides/What_Doctor_says';
+import What_Patients_says from './screens/slides/What_Patients_Say';
+import How_Bynocs_Work from './screens/slides/work';
+import Login from './screens/slides/Login';
+import Enquiry from './screens/Enquiry';
+import Home from './screens/homePage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BottomNavTabs from './screens/bottomNavigation';
+import Notifications from './screens/notifications';
+const stack = createNativeStackNavigator();
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  useEffect(() => {
+    fetchData();
+    checkLoginStatus();
+  }, []);
+
+  const checkLoginStatus = async () => {
+    try {
+      const status = await AsyncStorage.getItem('loggedIn');
+      if (status === 'true') {
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.log('Error checking login status:', error);
+    }
+  };
+  const fetchData = async () => {
+    try {
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  const MainEntry = ({navigation})=>{
+    return(<View style={{ flex: 1 }}>
+      {isLoading ? (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" color="#175CA4" />
+        </View>
+      ) : (
+        <Main navigation={navigation}/>
+      )}
+    </View>)
+  }
+
+  return (
+    <NavigationContainer>
+          <stack.Navigator>
+            <stack.Group>
+            <stack.Screen
+              component={!isLoggedIn ? MainEntry : BottomNavTabs}
+              name={!isLoggedIn ? "MainEntry":"BottomNavTabs"}
+              options={{
+                headerShown: false,
+              }}
+            />
+            {isLoggedIn?(
+              null
+            ):(
+              <stack.Screen component={BottomNavTabs} name="BottomNavTabs" options={{
+                headerShown:false
+              }}/>
+            )}
+            <stack.Screen component={Login} name="Login" options={{
+              headerShown:true
+            }}/>
+            <stack.Screen component={Enquiry} name="Enquiry" options={{
+              headerShown:true
+            }}/>
+            <stack.Screen component={Notifications} name="Notifications" options={{
+              headerShown:true
+            }}/>
+            </stack.Group>
+          </stack.Navigator>
+    </NavigationContainer>
+  )
+};
+
+export default App;
